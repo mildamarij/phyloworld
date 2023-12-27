d52bda46f71c7de723a420285b3b2e9446cb5e12
Milda Milčiutė <milda@vugene.eu>
2023-12-27 14:43:08 +0200

    Added descriptions, imporved code

--- phyloworld/create_tree.py
+++ phyloworld/create_tree.py
@@ -1,87 +1,146 @@
 import pandas as pd
-from Bio import Phylo
-import matplotlib.pyplot as plt
 from chart_studio import plotly
 import plotly.graph_objects as go
+import random
 
-AVAILABLE_COLORS = [
-    "rgb(31, 119, 180)", "rgb(255, 127, 14)", "rgb(44, 160, 44)",
-    "rgb(214, 39, 40)", "rgb(148, 103, 189)", "rgb(140, 86, 75)",
-    "rgb(227, 119, 194)", "rgb(127, 127, 127)", "rgb(188, 189, 34)",
-    "rgb(23, 190, 207)", "rgb(240, 228, 66)", "rgb(65, 244, 47)",
-    "rgb(502, 102, 152)", "rgb(204, 204, 204)", "rgb(200, 36, 17)",
-    "rgb(114, 147, 203)", "rgb(83, 81, 84)", "rgb(147, 160, 61)",
-    "rgb(169, 170, 68)", "rgb(193, 190, 70)", "rgb(93, 162, 233)"
-]
 
 def get_x_coordinates(tree):
+    """
+    Get the x-coordinates of tree branches.
+
+    Parameters:
+    - tree: Phylogenetic tree object.
+
+    Returns:
+    - xcoords: Dictionary of x-coordinates for each node in the tree.
+    """
     xcoords = tree.depths()
     if not max(xcoords.values()):
         xcoords = tree.depths(unit_branch_lengths=True)
+
     return xcoords
 
-def get_y_coordinates(tree, dist=1.3):
-    maxheight = tree.count_terminals() 
-    ycoords = dict((leaf, maxheight - i * dist) for i, leaf in enumerate(reversed(tree.get_terminals())))
+
+def get_y_coordinates(tree):
+    """
+    Get the y-coordinates of tree branches.
+
+    Parameters:
+    - tree: Phylogenetic tree object.
+
+    Returns:
+    - ycoords: Dictionary of y-coordinates for each terminal node in the tree.
+    """
+    maxheight = tree.count_terminals()
+    ycoords = dict(
+        (leaf, maxheight - i * 1.3)
+        for i, leaf in enumerate(reversed(tree.get_terminals()))
+    )
+
     def calc_row(clade):
         for subclade in clade:
             if subclade not in ycoords:
                 calc_row(subclade)
-        ycoords[clade] = (ycoords[clade.clades[0]] +
-                          ycoords[clade.clades[-1]]) / 2
+        ycoords[clade] = (ycoords[clade.clades[0]] + ycoords[clade.clades[-1]]) / 2
 
     if tree.root.clades:
         calc_row(tree.root)
+
     return ycoords
 
-            
-def get_clade_lines(orientation='horizontal', y_curr=0, x_start=0, x_curr=0, y_bot=0, y_top=0,
-                    line_color='rgb(25,25,25)', line_width=0.5):
-    branch_line = dict(type='line',
-                       layer='below',
-                       line=dict(color=line_color,
-                                 width=line_width)
-                       )
-    if orientation == 'horizontal':
-        branch_line.update(x0=x_start,
-                           y0=y_curr,
-                           x1=x_curr,
-                           y1=y_curr)
-    elif orientation == 'vertical':
-        branch_line.update(x0=x_curr,
-                           y0=y_bot,
-                           x1=x_curr,
-                           y1=y_top)
+
+def get_clade_lines(
+    orientation="horizontal",
+    y_curr=0,
+    x_start=0,
+    x_curr=0,
+    y_bot=0,
+    y_top=0,
+    line_color="rgb(25,25,25)",
+    line_width=0.5,
+):
+    """
+    Get the shape of a branch line for a clade.
+
+    Parameters:
+    - orientation: Orientation of the line ('horizontal' or 'vertical').
+    - y_curr: Current y-coordinate.
+    - x_start: Starting x-coordinate.
+    - x_curr: Ending x-coordinate.
+    - y_bot: Bottom y-coordinate for vertical lines.
+    - y_top: Top y-coordinate for vertical lines.
+    - line_color: Color of the line.
+    - line_width: Width of the line.
+
+    Returns:
+    - branch_line: Dictionary representing the branch line shape.
+    """
+    branch_line = dict(
+        type="line", layer="below", line=dict(color=line_color, width=line_width)
+    )
+    if orientation == "horizontal":
+        branch_line.update(x0=x_start, y0=y_curr, x1=x_curr, y1=y_curr)
+    elif orientation == "vertical":
+        branch_line.update(x0=x_curr, y0=y_bot, x1=x_curr, y1=y_top)
     else:
         raise ValueError("Line type can be 'horizontal' or 'vertical'")
     return branch_line
 
 
-def draw_clade(clade, x_start, line_shapes, annotations, line_color='rgb(15,15,15)', line_width=1, x_coords=0, y_coords=0, include_confidence=True):
+def draw_clade(
+    clade,
+    x_start,
+    line_shapes,
+    annotations,
+    line_color="rgb(15,15,15)",
+    line_width=1,
+    x_coords=0,
+    y_coords=0,
+    include_confidence=True,
+):
+    """
+    Recursively draw a clade and its descendants on the plot.
+
+    Parameters:
+    - clade: Phylogenetic tree clade.
+    - x_start: Starting x-coordinate for the clade.
+    - line_shapes: List to store shapes of branch lines.
+    - annotations: List to store annotations for confidence values.
+    - line_color: Color of the branch lines.
+    - line_width: Width of the branch lines.
+    - x_coords: Dictionary of x-coordinates for each node in the tree.
+    - y_coords: Dictionary of y-coordinates for each terminal node in the tree.
+    - include_confidence: Flag to include confidence values in annotations, default is TRUE.
+    """
     x_curr = x_coords[clade]
     y_curr = y_coords[clade]
-
-    branch_line = get_clade_lines(orientation='horizontal', y_curr=y_curr, x_start=x_start, x_curr=x_curr,
-                                  line_color=line_color, line_width=line_width)
+    branch_line = get_clade_lines(
+        orientation="horizontal",
+        y_curr=y_curr,
+        x_start=x_start,
+        x_curr=x_curr,
+        line_color=line_color,
+        line_width=line_width,
+    )
 
     line_shapes.append(branch_line)
-    
-    confidence = getattr(clade, 'confidence', None)
+
+    confidence = getattr(clade, "confidence", None)
     if confidence is not None and include_confidence:
         box_annotation = dict(
             xref="x",
             yref="y",
-            x=x_curr,  
-            y=y_curr,  
+            x=x_curr,
+            y=y_curr,
             showarrow=False,
-            bgcolor='rgba(255, 255, 255, 0.7)',
-            bordercolor='rgb(25,25,25)',
+            bgcolor="rgba(255, 255, 255, 0.7)",
+            bordercolor="rgb(25,25,25)",
             borderwidth=1,
             font=dict(size=6),
-            align='right',
-            xanchor='right',
-            yanchor='bottom',
-            text=f'{confidence:.2f}',
+            align="right",
+            xanchor="right",
+            yanchor="bottom",
+            text=f"{confidence:.2f}",
         )
 
         annotations.append(box_annotation)
@@ -90,24 +149,69 @@ def draw_clade(clade, x_start, line_shapes, annotations, line_color='rgb(15,15,1
         y_top = y_coords[clade.clades[0]]
         y_bot = y_coords[clade.clades[-1]]
 
-        line_shapes.append(get_clade_lines(orientation='vertical', x_curr=x_curr, y_bot=y_bot, y_top=y_top,
-                                           line_color=line_color, line_width=line_width))
+        line_shapes.append(
+            get_clade_lines(
+                orientation="vertical",
+                x_curr=x_curr,
+                y_bot=y_bot,
+                y_top=y_top,
+                line_color=line_color,
+                line_width=line_width,
+            )
+        )
 
         for child in clade:
-            draw_clade(child, x_curr, line_shapes, annotations, x_coords=x_coords, y_coords=y_coords, include_confidence=include_confidence)
+            draw_clade(
+                child,
+                x_curr,
+                line_shapes,
+                annotations,
+                x_coords=x_coords,
+                y_coords=y_coords,
+                include_confidence=include_confidence,
+            )
 
 
-def create_plot(tree, x_coords, y_coords, metadata, title, colors=AVAILABLE_COLORS, include_confidence=True):
+def create_plot(
+    tree, x_coords, y_coords, metadata, title, colors, include_confidence=True
+):
+    """
+    Create a phylogenetic tree plot.
+
+    Parameters:
+    - tree: Phylogenetic tree object.
+    - x_coords: Dictionary of x-coordinates for each node in the tree.
+    - y_coords: Dictionary of y-coordinates for each terminal node in the tree.
+    - metadata: DataFrame containing metadata with a "Country" column.
+    - title: Title of the plot.
+    - colors: List of possible color codes (hexadecimal or other), by default random colours are generated.
+    - include_confidence: Flag to include confidence values in annotations, default is TRUE.
+
+    Returns:
+    - fig: Plotly figure object.
+    """
     line_shapes = []
     annotations = []
 
-    draw_clade(tree.root, 0, line_shapes, annotations, line_color='rgb(25,25,25)', line_width=1, x_coords=x_coords, y_coords=y_coords, include_confidence=include_confidence)
+    draw_clade(
+        tree.root,
+        0,
+        line_shapes,
+        annotations,
+        line_color="rgb(25,25,25)",
+        line_width=1,
+        x_coords=x_coords,
+        y_coords=y_coords,
+        include_confidence=include_confidence,
+    )
 
     X, Y, text, color = [], [], [], []
 
     color_map = generate_country_color_map(metadata, colors)
     label_legend = set(metadata["Country"].unique())
-    color_scale = {country: color_map.get(country, 'rgb(100,100,100)') for country in label_legend}
+    color_scale = {
+        country: color_map.get(country, "rgb(100,100,100)") for country in label_legend
+    }
 
     for cl in x_coords.keys():
         if cl.is_terminal():
@@ -115,51 +219,90 @@ def create_plot(tree, x_coords, y_coords, metadata, title, colors=AVAILABLE_COLO
             Y.append(y_coords[cl])
             node_id = cl.name
             country = metadata.loc[metadata["ID"] == node_id, "Country"].values[0]
-            text.append(f'Country: {country}<br>ID: {node_id}')
+            text.append(f"Country: {country}<br>ID: {node_id}")
             color.append(color_scale[country])
 
     trace = go.Scatter(
         x=X,
         y=Y,
-        mode='markers',
+        mode="markers",
         marker=dict(color=color, size=5),
         text=text,
-        hoverinfo='text',
-        name='Countries'
+        hoverinfo="text",
+        name="Countries",
     )
 
     layout = go.Layout(
         title=title,
-        paper_bgcolor='rgba(0,0,0,0)',
-        xaxis=dict(title='Branch Length'),
+        paper_bgcolor="rgba(0,0,0,0)",
+        xaxis=dict(title="Branch Length"),
         yaxis=dict(
             showline=False,
             zeroline=False,
             showgrid=False,
             showticklabels=False,
-            title=''
+            title="",
         ),
-        hovermode='closest',
+        hovermode="closest",
         shapes=line_shapes,
-        plot_bgcolor='rgb(250,250,250)',
+        plot_bgcolor="rgb(250,250,250)",
         legend=dict(x=0, y=1),
-        annotations=annotations 
+        annotations=annotations,
     )
 
     fig = go.Figure(data=[trace], layout=layout)
     return fig
 
-def generate_country_color_map(metadata, colors=AVAILABLE_COLORS):
-    unique_countries = metadata["Country"].unique()
-    color_map = {}
 
-    for i, country in enumerate(unique_countries):
-        color_map[country] = colors[i % len(colors)]
+def generate_country_color_map(metadata, colors=None):
+    """
+    Generate a mapping of unique countries to random or custom provided colors.
+
+    Parameters:
+    - metadata (pd.DataFrame): A DataFrame containing metadata with a "Country" column.
+    - colors (list, optional): A list of possible color codes (hexadecimal or other).
+
+    Returns:
+    - color_map (dict): A mapping of countries to colors.
+    """
+    random.seed(927)
+    unique_countries = metadata["Country"].unique()
+    num_countries = len(unique_countries)
+
+    if colors:
+        if len(colors) < num_countries:
+            raise ValueError(
+                "Not enough possible colors provided for unique countries."
+            )
+        color_map = dict(zip(unique_countries, colors[:num_countries]))
+    else:
+        random_colors = [
+            (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            for _ in range(num_countries)
+        ]
+        random_colors_hex = ["#%02x%02x%02x" % rgb for rgb in random_colors]
+        color_map = dict(zip(unique_countries, random_colors_hex))
 
     return color_map
 
-def create_phylotree(tree, metadata, title = "", colors = AVAILABLE_COLORS, include_confidence=True):
+
+def create_phylotree(tree, metadata, title="", colors=None, include_confidence=True):
+    """
+    Create a phylogenetic tree plot with annotations and customizable styling.
+
+    Parameters:
+    - tree: Phylogenetic tree object.
+    - metadata: DataFrame containing metadata with a "Country" column.
+    - title: Title of the plot.
+    - colors: List of possible color codes (hexadecimal or other). by default random colours are generated.
+    - include_confidence: Flag to include confidence values in annotations, default is TRUE.
+
+    Returns:
+    - fig: Plotly figure object.
+    """
     x_coords = get_x_coordinates(tree)
     y_coords = get_y_coordinates(tree)
-    fig = create_plot(tree, x_coords, y_coords, metadata, title, colors, include_confidence)
+    fig = create_plot(
+        tree, x_coords, y_coords, metadata, title, colors, include_confidence
+    )
     return fig
--- phyloworld/get_coordinates.py
+++ phyloworld/get_coordinates.py
@@ -2,6 +2,18 @@ import pandas as pd
 import requests
 
 def get_country_coordinates(country):
+    """
+    Retrieve latitude and longitude coordinates for a specified
+    country using the Nominatim OpenStreetMap API.
+
+    Parameters:
+    - country (str): The name of the country for which
+    coordinates are to be obtained.
+
+    Returns:
+    - output (list): A list containing the latitude and
+    longitude coordinates for the specified country.
+    """
     print(f"Getting coordinates for {country}")
     url = "{0}{1}{2}".format(
         "http://nominatim.openstreetmap.org/search?country=",
@@ -11,18 +23,29 @@ def get_country_coordinates(country):
     response = requests.get(url).json()[0]
     coordinates = [response.get(key) for key in ["lat", "lon"]]
     output = [float(i) for i in coordinates]
+    
     return output
 
 def merge_coordinates_with_metadata(metadata):
-    coordinates = pd.DataFrame(columns=["Country", "Latitude", "Longitude"])
-    countries = set(metadata["Country"])
-    for country in countries:
+    """
+    Merge metata with latitude and longitude coordinates for each
+    unique country present in the "Country" column.
+
+    Parameters:
+    - metadata (pd.DataFrame): A DataFrame containing metadata with a "Country" column.
+
+    Returns:
+    - merged_metadata (pd.DataFrame): The input metadata DataFrame with additional columns
+    for latitude and longitude coordinates corresponding to each country.
+    """
+    country_data_list = []
+    for country in set(metadata["Country"]):
         country_name = country.lower().replace(" ", "+")
         coordinates_list = get_country_coordinates(country_name)
-        country_data = pd.DataFrame(
-            [[country, coordinates_list[0], coordinates_list[1]]],
-            columns=["Country", "Latitude", "Longitude"]
-        )
-        coordinates = pd.concat([coordinates, country_data], ignore_index=True)
-    merged_metadata = metadata.merge(coordinates, on="Country")
+        country_data_list.append([country, coordinates_list[0], coordinates_list[1]])
+    country_data = pd.DataFrame(
+        country_data_list, columns=["Country", "Latitude", "Longitude"]
+    )
+    merged_metadata = metadata.merge(country_data, on="Country")
+
     return merged_metadata
